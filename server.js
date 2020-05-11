37958c4f89264cc7659c915601ce8d2c11d03c25
SebasCasti00 <sebas_castillo000826@hotmail.com>
2020-05-11 08:23:27 -0500

    Lets get started

--- server.js
+++ server.js
@@ -1,16 +1,43 @@
 const express = require('express');
 const exphs = require('express-handlebars');
+/*
+const bodyParser = require('body-parser');
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const createRoutes = require('./routes.js');
+
 
 const app = express();
 
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+
 app.engine('handlebars', exphs());
-app.set('view engine','handlebars');
+app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res){
 
-    res.send('hola');
+MONGO WTF OJO CON EL LOCALHOST ACA..
+const url= 'mongo://localhost:27017;
+const dbName = 'store';
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true});
+
+
+client.connect(function(err){
+    assert.equal(null, err);
+
+    console.log('conexión');
+
+    const db = client.db(dbName);
+
+    createRoutes(app, db);
+
+    app.listen(3000, ()=>{
+        console.log("Iniciado puerto 3k");
+    });
 });
+*/
 
-app.listen(3000, function(){
-    console.log('aiuda.');
-});
+const app = express();
+
+
